e29d1674418ade29aeda964925989bfbe90c785b
PumpkinXD <54535387+PumpkinXD@users.noreply.github.com>
2024-08-14 15:33:28 +0800

    hcaptcha and qc code not working atm

--- main.js
+++ main.js
@@ -2,8 +2,23 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
-app.commandLine.appendSwitch('host-rules', 'MAP discord.com dis1, MAP discordapp.com dis2, MAP discord.gg dis3')//https://nicebowl.fun/24_8
-app.commandLine.appendSwitch('host-resolver-rules', 'MAP dis1 162.159.138.232, MAP dis2 162.159.129.233, MAP dis3 162.159.130.234')//https://www.diggui.com
+app.commandLine.appendSwitch('host-rules',
+                             'MAP discord.com ez4dc,'
+                             +'MAP discordapp.com ez4dc,'
+                             +'MAP discord.gg ez4dc,'
+                             +'MAP remote-auth-gateway.discord.gg ez4dc,'
+                             +'MAP *.hcaptcha.com ez4hcaptcha'//not working
+                             )//https://nicebowl.fun/24_8
+app.commandLine.appendSwitch('host-resolver-rules',
+                             ' MAP ez4dc 162.159.138.232,'
+                             +' MAP dis2 162.159.129.233,'
+                             +' MAP ez4dc_3 162.159.130.234,'
+                             +' MAP ez4hcaptcha 104.19.230.21'//not working
+                             // +' MAP api2.hcaptcha.com 104.19.230.21,'
+                             // +' MAP hcaptcha.com 104.19.230.21,'
+                             // +' MAP newassets.hcaptcha.com 104.19.230.21,'
+                             // +' MAP imgs.hcaptcha.com 104.19.230.21'
+                             )//https://www.diggui.com
 app.commandLine.appendSwitch('test-type')
 app.commandLine.appendSwitch('ignore-certificate-errors')
 
@@ -22,7 +37,7 @@ function createWindow () {
 
   mainWindow.maximize();
   mainWindow.setAutoHideMenuBar(true);
-  mainWindow.loadURL('https://discord.com/channels');
+  mainWindow.loadURL('https://discord.com/app');
   // mainWindow.loadURL('https://baidu.com');
 
   // and load the index.html of the app.
